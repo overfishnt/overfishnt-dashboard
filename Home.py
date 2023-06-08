b2b15a35f42d14c674eeda49c342ecfa995f2a7f
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-06-08 20:59:03 +0700

    Add Content

--- Home.py
+++ Home.py
@@ -12,7 +12,9 @@ st.set_page_config(
 # Styling the page
 with open("styles/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-st.title("FISHERY DATA DASHBOARD")
+st.image("https://fishku.id/assets/img/logo-fishku.png")
+
+st.title("")
 st.subheader("**Fishku**")
 st.write(
     "Fishku merupakan startup e-commerce di bidang perikanan yang bertujuan untuk mempermudah kegiatan jual beli ikan dan mendeteksi kesegaran ikan menggunakan machine learning."

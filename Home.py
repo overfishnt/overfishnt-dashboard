63963d0f72a031bf60dcbf011f5754439ba3fc30
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-06-08 20:13:43 +0700

    Add hompage content

--- Home.py
+++ Home.py
@@ -8,7 +8,16 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
 # Styling the page
 with open("styles/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-st.title("FISHKU")
+st.title("FISHERY DATA DASHBOARD")
+st.subheader("**Fishku**")
+st.write(
+    "Fishku merupakan startup e-commerce di bidang perikanan yang bertujuan untuk mempermudah kegiatan jual beli ikan dan mendeteksi kesegaran ikan menggunakan machine learning."
+)
+st.subheader("**Overfishn't**")
+st.write(
+    "Overfishn't adalah tim yang terbentuk dari Company Capstone Bangkit 2023 bertujuan membuat dashboard untuk membantu para nelayan dalam berlabuh dengan memprediksi beberapa hal yang berkaitan dengan oceanografi."
+)

c1c828ca6d45f2e31c8f0fb33604c572465c0ab0
nicokangdra <99784038+nicokangdra@users.noreply.github.com>
2023-06-08 22:54:26 +0700

    Revise

--- Home.py
+++ Home.py
@@ -23,3 +23,10 @@ st.subheader("**Overfishn't**")
 st.write(
     "Overfishn't adalah tim yang terbentuk dari Company Capstone Bangkit 2023 bertujuan membuat dashboard untuk membantu para nelayan dalam berlabuh dengan memprediksi beberapa hal yang berkaitan dengan oceanografi."
 )
+st.title("")
+st.title("")
+st.title("WPP RI")
+st.write(
+    "**Indonesia memiliki 11 Wilayah Pengelolaan Perikanan (WPP) yang merupakan area kegiatan perikanan, seperti: penangkapan ikan, pembudidayaan ikan, konservasi, penelitian, dan pengembangan perikanan yang meliputi perairan pedalaman, perairan kepulauan, laut teritorial, zona tambahan, dan zona ekonomi eksklusif Indonesia (Peraturan Pemerintah No.18 Tahun 2014).**"
+)
+
